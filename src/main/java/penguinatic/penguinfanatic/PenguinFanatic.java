722864db7e16cbff4fcc6b847238eddf8e6999c1
Penguinatic <01richardsonalfie@gmail.com>
2021-03-29 15:08:54 +0100

    Initial commit

---
+++ src/main/java/penguinatic/penguinfanatic/PenguinFanatic.java
@@ -0,0 +1,10 @@
+package penguinatic.penguinfanatic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PenguinFanatic implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/penguinatic/penguinfanatic/client/PenguinFanaticClient.java
@@ -0,0 +1,13 @@
+package penguinatic.penguinfanatic.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PenguinFanaticClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
