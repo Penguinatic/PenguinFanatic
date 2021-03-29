a1ea19b58c045c3b763dd8e81692fba6e4bcd45f
Penguinatic <01richardsonalfie@gmail.com>
2021-03-29 16:30:21 +0100

    Added in the Walking Stick and the Disappearing Ice it generates

--- src/main/java/penguinatic/penguinfanatic/PenguinFanatic.java
+++ src/main/java/penguinatic/penguinfanatic/PenguinFanatic.java
@@ -3,13 +3,16 @@ package penguinatic.penguinfanatic;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.Item;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Material;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import penguinatic.penguinfanatic.block.DisappearingIce;
 import penguinatic.penguinfanatic.items.PenguinFeatherItem;
+import penguinatic.penguinfanatic.items.WalkingStickItem;
 
 public class PenguinFanatic implements ModInitializer {
 
@@ -24,13 +27,17 @@ public class PenguinFanatic implements ModInitializer {
         registerItems();
     }
 
-    public void registerBlocks() {
+    public static final DisappearingIce DISAPPEARING_ICE = new DisappearingIce(AbstractBlock.Settings.of(Material.ICE).slipperiness(0.98F).ticksRandomly().strength(0.5F).sounds(BlockSoundGroup.GLASS).nonOpaque());
 
+    public void registerBlocks() {
+        Registry.register(Registry.BLOCK, new Identifier("penguinfanatic", "disappearing_ice"), DISAPPEARING_ICE);
     }
 
     public static final PenguinFeatherItem PENGUIN_FEATHER = new PenguinFeatherItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    public static final WalkingStickItem WALKING_STICK = new WalkingStickItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
 
     public void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_feather"), PENGUIN_FEATHER);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "walking_stick"), WALKING_STICK);
     }
 }
---
+++ src/main/java/penguinatic/penguinfanatic/items/WalkingStickItem.java
@@ -0,0 +1,48 @@
+package penguinatic.penguinfanatic.items;
+
+import penguinatic.penguinfanatic.PenguinFanatic;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class WalkingStickItem extends Item {
+
+    public WalkingStickItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        CompoundTag tag = new CompoundTag();
+
+        tag.putBoolean("on", false);
+
+        if (stack.hasTag()) {
+            tag = stack.getTag();
+        }
+
+        tag.putBoolean("on", !tag.getBoolean("on"));
+        stack.setTag(tag);
+
+        return TypedActionResult.success(user.getStackInHand(hand));
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
+
+        if (stack.hasTag() && stack.getTag().getBoolean("on")
+                && (world.getBlockState(entity.getBlockPos().down()).getBlock() == Blocks.WATER && world.getBlockState(entity.getBlockPos().down()).getFluidState().isStill()
+                || world.getBlockState(entity.getBlockPos().down()).getBlock() == Blocks.AIR)
+                && !entity.isSneaking()) {
+            world.setBlockState(entity.getBlockPos().down(), PenguinFanatic.DISAPPEARING_ICE.getDefaultState());
+        }
+    }
+}
