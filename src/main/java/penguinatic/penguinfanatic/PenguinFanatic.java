464aef193697ed69e8853cd5df48b6d43bfc7adb
Penguinatic <01richardsonalfie@gmail.com>
2021-04-05 19:34:07 +0100

    Items now have a registry class.

--- src/main/java/penguinatic/penguinfanatic/PenguinFanatic.java
+++ src/main/java/penguinatic/penguinfanatic/PenguinFanatic.java
@@ -4,39 +4,28 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
-import penguinatic.penguinfanatic.items.PenguinFeatherItem;
-import penguinatic.penguinfanatic.items.WalkingStickItem;
 import penguinatic.penguinfanatic.registry.BlocksRegistry;
 import penguinatic.penguinfanatic.registry.EntityRegistry;
+import penguinatic.penguinfanatic.registry.ItemsRegistry;
 
 public class PenguinFanatic implements ModInitializer {
 
     public static final ItemGroup PENGUIN_FANATIC_GROUP = FabricItemGroupBuilder.build(
             new Identifier("penguinfanatic", "general"),
-            () -> new ItemStack(PenguinFanatic.PENGUIN_FEATHER)
+            () -> new ItemStack(ItemsRegistry.PENGUIN_FEATHER)
     );
 
     @Override
     public void onInitialize() {
         BlocksRegistry.register();
-        registerItems();
+        ItemsRegistry.register();
         new EntityRegistry().register();
         initSpawns();
-    }
-
-    public static final PenguinFeatherItem PENGUIN_FEATHER = new PenguinFeatherItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
-    public static final WalkingStickItem WALKING_STICK = new WalkingStickItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
-
-    public void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_feather"), PENGUIN_FEATHER);
-        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "walking_stick"), WALKING_STICK);
     }
 
     private static void initSpawns() {
---
+++ src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
@@ -0,0 +1,20 @@
+package penguinatic.penguinfanatic.registry;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import penguinatic.penguinfanatic.PenguinFanatic;
+import penguinatic.penguinfanatic.items.PenguinFeatherItem;
+import penguinatic.penguinfanatic.items.WalkingStickItem;
+
+public class ItemsRegistry {
+
+    public static final PenguinFeatherItem PENGUIN_FEATHER = new PenguinFeatherItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    public static final WalkingStickItem WALKING_STICK = new WalkingStickItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_feather"), PENGUIN_FEATHER);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "walking_stick"), WALKING_STICK);
+    }
+
+}
