4081e1ac876aad85c933c0efebcc4badeedd071a
Penguinatic <01richardsonalfie@gmail.com>
2021-04-06 17:08:34 +0100

    Added the penguin armour set

---
+++ src/main/java/penguinatic/penguinfanatic/materials/PenguinArmorMaterial.java
@@ -0,0 +1,55 @@
+package penguinatic.penguinfanatic.materials;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import penguinatic.penguinfanatic.registry.ItemsRegistry;
+
+public class PenguinArmorMaterial implements ArmorMaterial {
+
+    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {2, 5, 6, 2};
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()] * 15;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_VALUES[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 25;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_IRON;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ItemsRegistry.PENGUIN_STEEL);
+    }
+
+    @Override
+    public String getName() {
+        return "penguin";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+
+}
--- src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
+++ src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
@@ -1,11 +1,15 @@
 package penguinatic.penguinfanatic.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import penguinatic.penguinfanatic.PenguinFanatic;
 import penguinatic.penguinfanatic.items.WalkingStickItem;
+import penguinatic.penguinfanatic.materials.PenguinArmorMaterial;
 
 public class ItemsRegistry {
 
@@ -14,11 +18,22 @@ public class ItemsRegistry {
 
     public static final WalkingStickItem WALKING_STICK = new WalkingStickItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
 
+    public static final ArmorMaterial PENGUIN_ARMOR_MATERIAL = new PenguinArmorMaterial();
+    public static final Item PENGUIN_HELMET = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    public static final Item PENGUIN_CHESTPLATE = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    public static final Item PENGUIN_LEGGINGS = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    public static final Item PENGUIN_BOOTS = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_feather"), PENGUIN_FEATHER);
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_steel"), PENGUIN_STEEL);
 
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "walking_stick"), WALKING_STICK);
+
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_helmet"), PENGUIN_HELMET);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_chestplate"), PENGUIN_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_leggings"), PENGUIN_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_boots"), PENGUIN_BOOTS);
     }
 
 }
