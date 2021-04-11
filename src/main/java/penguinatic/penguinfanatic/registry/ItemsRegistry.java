b6103e35860112c07a0f2c1c86ce19e2e3ef34e9
Penguinatic <01richardsonalfie@gmail.com>
2021-04-11 19:40:16 +0100

    Commented out armor to test branches

--- src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
+++ src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
@@ -18,11 +18,11 @@ public class ItemsRegistry {
 
     public static final IcePathGemstoneItem ICE_PATH_GEMSTONE = new IcePathGemstoneItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP).maxDamage(5));
 
-    public static final ArmorMaterial PENGUIN_ARMOR_MATERIAL = new PenguinArmorMaterial();
-    public static final Item PENGUIN_HELMET = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
-    public static final Item PENGUIN_CHESTPLATE = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
-    public static final Item PENGUIN_LEGGINGS = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
-    public static final Item PENGUIN_BOOTS = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    //public static final ArmorMaterial PENGUIN_ARMOR_MATERIAL = new PenguinArmorMaterial();
+    //public static final Item PENGUIN_HELMET = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    //public static final Item PENGUIN_CHESTPLATE = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    //public static final Item PENGUIN_LEGGINGS = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    //public static final Item PENGUIN_BOOTS = new ArmorItem(PENGUIN_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Settings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_feather"), PENGUIN_FEATHER);
@@ -30,10 +30,10 @@ public class ItemsRegistry {
 
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "ice_path_gemstone"), ICE_PATH_GEMSTONE);
 
-        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_helmet"), PENGUIN_HELMET);
-        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_chestplate"), PENGUIN_CHESTPLATE);
-        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_leggings"), PENGUIN_LEGGINGS);
-        Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_boots"), PENGUIN_BOOTS);
+        //Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_helmet"), PENGUIN_HELMET);
+        //Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_chestplate"), PENGUIN_CHESTPLATE);
+        //Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_leggings"), PENGUIN_LEGGINGS);
+        //Registry.register(Registry.ITEM, new Identifier("penguinfanatic", "penguin_boots"), PENGUIN_BOOTS);
     }
 
 }
