cfe1fc5e7b7326af803918f75f53dadf8e4aea8e
Penguinatic <01richardsonalfie@gmail.com>
2021-04-15 15:42:45 +0100

    New cuter penguin model!

--- src/main/java/penguinatic/penguinfanatic/entity/model/PenguinEntityModel.java
+++ src/main/java/penguinatic/penguinfanatic/entity/model/PenguinEntityModel.java
@@ -2,7 +2,7 @@
 	// Exported for Minecraft version 1.15
 	// Paste this class into your mod and generate all required imports
 
-	package penguinatic.penguinfanatic.entity.model;
+package penguinatic.penguinfanatic.entity.model;
 
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
@@ -12,84 +12,74 @@ import net.minecraft.entity.Entity;
 import penguinatic.penguinfanatic.entity.PenguinEntity;
 
 public class PenguinEntityModel extends EntityModel<PenguinEntity> {
-private final ModelPart upper_body;
-	private final ModelPart left_arm_upper;
-	private final ModelPart left_arm_lower;
-	private final ModelPart right_arm_upper;
-	private final ModelPart right_arm_lower;
+
+	private final ModelPart body;
+	private final ModelPart right_flipper;
+	private final ModelPart cube_r1;
+	private final ModelPart left_flipper;
 	private final ModelPart head;
 	private final ModelPart beak;
-	private final ModelPart lower_body;
-	private final ModelPart right_leg;
-	private final ModelPart right_flipper;
-	private final ModelPart left_leg;
-	private final ModelPart left_flipper;
+	private final ModelPart left_arm;
+	private final ModelPart cube_r2;
+	private final ModelPart right_arm;
+	private final ModelPart cube_r3;
+
 public PenguinEntityModel() {
 		textureWidth = 64;
 		textureHeight = 64;
-		upper_body = new ModelPart(this);
-		upper_body.setPivot(0.0F, 5.0F, 1.0F);
-		upper_body.setTextureOffset(0, 17).addCuboid(-4.0F, 0.0F, -3.0F, 8.0F, 8.0F, 6.0F, 0.0F, false);
+		body = new ModelPart(this);
+		body.setPivot(-1.0F, 23.0F, -2.0F);
+		body.setTextureOffset(0, 0).addCuboid(-5.0F, -12.0F, -3.0F, 12.0F, 11.0F, 10.0F, 0.0F, false);
 
-		left_arm_upper = new ModelPart(this);
-		left_arm_upper.setPivot(4.0F, 1.0F, 0.0F);
-		upper_body.addChild(left_arm_upper);
-		setRotationAngle(left_arm_upper, 0.0F, 0.0F, -0.2182F);
-		left_arm_upper.setTextureOffset(10, 31).addCuboid(-0.5F, -0.5F, -2.0F, 1.0F, 6.0F, 4.0F, 0.0F, false);
+		right_flipper = new ModelPart(this);
+		right_flipper.setPivot(3.0F, 1.0F, 0.0F);
+		body.addChild(right_flipper);
+		
 
-		left_arm_lower = new ModelPart(this);
-		left_arm_lower.setPivot(-4.5F, 17.5F, -1.0F);
-		left_arm_upper.addChild(left_arm_lower);
-		left_arm_lower.setTextureOffset(20, 36).addCuboid(4.0F, -12.0F, 0.0F, 1.0F, 5.0F, 3.0F, 0.0F, false);
+		cube_r1 = new ModelPart(this);
+		cube_r1.setPivot(-6.5F, -1.0F, -1.0F);
+		right_flipper.addChild(cube_r1);
+		setRotationAngle(cube_r1, 0.0F, 0.3927F, 0.0F);
+		cube_r1.setTextureOffset(34, 0).addCuboid(-2.5F, -1.0F, -4.0F, 5.0F, 2.0F, 8.0F, 0.0F, false);
 
-		right_arm_upper = new ModelPart(this);
-		right_arm_upper.setPivot(-3.75F, 1.5F, 0.0F);
-		upper_body.addChild(right_arm_upper);
-		setRotationAngle(right_arm_upper, 0.0F, 0.0F, 0.1745F);
-		right_arm_upper.setTextureOffset(0, 31).addCuboid(-1.0F, -1.0F, -2.0F, 1.0F, 6.0F, 4.0F, 0.0F, false);
-
-		right_arm_lower = new ModelPart(this);
-		right_arm_lower.setPivot(-4.0F, 16.75F, -1.0F);
-		right_arm_upper.addChild(right_arm_lower);
-		right_arm_lower.setTextureOffset(0, 0).addCuboid(3.0F, -12.0F, 0.0F, 1.0F, 5.0F, 3.0F, 0.0F, false);
+		left_flipper = new ModelPart(this);
+		left_flipper.setPivot(4.5F, 0.0F, -1.0F);
+		body.addChild(left_flipper);
+		setRotationAngle(left_flipper, 0.0F, -0.3927F, 0.0F);
+		left_flipper.setTextureOffset(24, 28).addCuboid(-1.5761F, -1.0F, -4.3827F, 5.0F, 2.0F, 8.0F, 0.0F, false);
 
 		head = new ModelPart(this);
-		head.setPivot(0.0F, 0.0F, 0.0F);
-		upper_body.addChild(head);
-		head.setTextureOffset(23, 26).addCuboid(-3.0F, -5.0F, -3.0F, 6.0F, 5.0F, 5.0F, 0.0F, false);
+		head.setPivot(1.0F, -12.0F, 3.0F);
+		body.addChild(head);
+		head.setTextureOffset(0, 21).addCuboid(-4.0F, -7.0F, -5.0F, 8.0F, 7.0F, 8.0F, 0.0F, false);
 
 		beak = new ModelPart(this);
 		beak.setPivot(0.0F, 0.0F, 0.0F);
 		head.addChild(beak);
-		beak.setTextureOffset(6, 31).addCuboid(-1.0F, -3.0F, -5.0F, 2.0F, 2.0F, 2.0F, 0.0F, false);
+		beak.setTextureOffset(24, 21).addCuboid(-2.0F, -3.0F, -8.0F, 4.0F, 2.0F, 3.0F, 0.0F, false);
 
-		lower_body = new ModelPart(this);
-		lower_body.setPivot(-1.0F, 18.0F, -3.0F);
-		upper_body.addChild(lower_body);
-		lower_body.setTextureOffset(0, 0).addCuboid(-4.0F, -10.0F, -1.0F, 10.0F, 9.0F, 8.0F, 0.0F, false);
+		left_arm = new ModelPart(this);
+		left_arm.setPivot(7.5F, -7.5F, 2.0F);
+		body.addChild(left_arm);
+		
 
-		right_leg = new ModelPart(this);
-		right_leg.setPivot(-2.0F, 1.0F, 0.0F);
-		lower_body.addChild(right_leg);
-		right_leg.setTextureOffset(22, 20).addCuboid(-1.0F, -2.0F, 2.0F, 3.0F, 1.0F, 2.0F, 0.0F, false);
+		cube_r2 = new ModelPart(this);
+		cube_r2.setPivot(0.0F, 0.0F, 0.0F);
+		left_arm.addChild(cube_r2);
+		setRotationAngle(cube_r2, 0.0F, 0.0F, -0.2618F);
+		cube_r2.setTextureOffset(14, 36).addCuboid(-0.5F, -4.5F, -3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
 
-		right_flipper = new ModelPart(this);
-		right_flipper.setPivot(5.0F, 0.0F, 0.0F);
-		right_leg.addChild(right_flipper);
-		right_flipper.setTextureOffset(28, 0).addCuboid(-6.0F, -1.0F, -2.0F, 3.0F, 1.0F, 6.0F, 0.0F, false);
+		right_arm = new ModelPart(this);
+		right_arm.setPivot(7.5F, -7.5F, 2.0F);
+		body.addChild(right_arm);
+		
 
-		left_leg = new ModelPart(this);
-		left_leg.setPivot(1.0F, -18.0F, 3.0F);
-		lower_body.addChild(left_leg);
-		left_leg.setTextureOffset(22, 17).addCuboid(1.0F, 17.0F, -1.0F, 3.0F, 1.0F, 2.0F, 0.0F, false);
-
-		left_flipper = new ModelPart(this);
-		left_flipper.setPivot(2.0F, 19.0F, -3.0F);
-		left_leg.addChild(left_flipper);
-		left_flipper.setTextureOffset(28, 17).addCuboid(-1.0F, -1.0F, -2.0F, 3.0F, 1.0F, 6.0F, 0.0F, false);
+		cube_r3 = new ModelPart(this);
+		cube_r3.setPivot(-13.0F, 0.0F, 0.0F);
+		right_arm.addChild(cube_r3);
+		setRotationAngle(cube_r3, 0.0F, 0.0F, 0.2618F);
+		cube_r3.setTextureOffset(0, 36).addCuboid(-0.5F, -4.5F, -3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
 }
-
-
 
 	@Override
 public void setAngles(PenguinEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
@@ -98,7 +88,7 @@ public void setAngles(PenguinEntity entity, float limbSwing, float limbSwingAmou
 @Override
 public void render(MatrixStack matrixStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
 		
-		upper_body.render(matrixStack, buffer, packedLight, packedOverlay);
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
 }
 public void setRotationAngle(ModelPart bone, float x, float y, float z) {
 		bone.pitch = x;
