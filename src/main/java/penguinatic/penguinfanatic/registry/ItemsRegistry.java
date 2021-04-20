67c9fe12579477512e7ec53640e650fa1bd20429
Penguinatic <01richardsonalfie@gmail.com>
2021-04-20 23:42:23 +0100

    Added squid and made it cookable.

--- src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
+++ src/main/java/penguinatic/penguinfanatic/registry/ItemsRegistry.java
@@ -1,6 +1,7 @@
 package penguinatic.penguinfanatic.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -11,10 +12,14 @@ public class ItemsRegistry {
 
     public static final Item PENGUIN_FEATHER = new Item(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
     public static final PenguinEggItem PENGUIN_EGG = new PenguinEggItem(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP));
+    public static final Item SQUID_RAW = new Item(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3F).meat().build()));
+    public static final Item SQUID_COOKED = new Item(new FabricItemSettings().group(PenguinFanatic.PENGUIN_FANATIC_GROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(0.6F).meat().build()));
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_feather"), PENGUIN_FEATHER);
         Registry.register(Registry.ITEM, new Identifier("penguinfanatic","penguin_egg"), PENGUIN_EGG);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic","squid_raw"), SQUID_RAW);
+        Registry.register(Registry.ITEM, new Identifier("penguinfanatic","squid_cooked"), SQUID_COOKED);
     }
 
 }
